de7a116dd0539b82d9ccdc26dd2f912ad27c501a
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-09-22 00:48:04 +0100

    add

--- div.c
+++ div.c
@@ -1,28 +1,34 @@
 #include "monty.h"
 
 /**
- * _div - Divides the second top element of the stack by the top element
- * @head: A pointer to the top of the stack
- * @line_number: The current line number of the bytecode file
- * Return: Nothing
+ * divide_top_two_elements - Divides the second top element by the top element of the stack.
+ * @stack_head: A pointer to the top of the stack.
+ * @line_number: The current line number of the bytecode file.
+ *
+ * Description:
+ * This function divides the second top element of the stack by the top element
+ * and replaces the second top element with the result. If the stack is too short
+ * or if division by zero is attempted, it prints corresponding error messages.
  */
-void _div(stack_t **head, unsigned int line_number)
+void divide_top_two_elements(stack_t **stack_head, unsigned int line_number)
 {
-	int div = 0;
+	int quotient = 0;
 
-	if (*head == 0 || (*head)->next == 0)
+	if (!stack_head || !*stack_head || !(*stack_head)->next)
 	{
 		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
 		error = 1;
 		return;
 	}
-	if ((*head)->n == 0)
+
+	if ((*stack_head)->n == 0)
 	{
 		fprintf(stderr, "L%d: division by zero\n", line_number);
 		error = 1;
 		return;
 	}
-	div = (((*head)->next)->n) / ((*head)->n);
-	_pop(head, line_number);
-	(*head)->n = div;
+
+	quotient = ((*stack_head)->next->n) / ((*stack_head)->n);
+	pop_top_element(stack_head, line_number);
+	(*stack_head)->n = quotient;
 }
