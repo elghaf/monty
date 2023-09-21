c502484e04ea6131a5ad41c6b1067a3194468c92
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-09-22 00:36:30 +0100

    add

--- main.c
+++ main.c
@@ -1,50 +1,86 @@
 #include "monty.h"
 
 /**
- * main - This is the entry point of a program
- * @ac: This is the number of arguments at the start of the program
- * @av: This is the array of characters at the start of the program
- * Return: Always 0 success
+ * main - monty interperter
+ * @ac: the number of arguments
+ * @av: the arguments
+ * Return: void
  */
 int main(int ac, char *av[])
 {
-	char *s = NULL, *buffer, *op_arr[2];
-	stack_t *h = NULL;
-	size_t buffersize = 1024, num_line = 0;
-	ssize_t line_put;
-	void (*func_op)(stack_t **staues, unsigned int count_line);
+        stack_t *stack = NULL;
+        static char *string[1000] = {NULL};
+        int n = 0;
+        FILE *fd;
+        size_t bufsize = 1000;
 
-	if (ac != 2)
-		fprintf(stderr, "USAGE: monty file\n");
-	exit(EXIT_FAILURE);
-	file = fopen(av[1], "r");
-	if (file == NULL)
-		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
-	exit(EXIT_FAILURE);
-	while (1)
-	{
-		line_put = getline(&s, &buffersize, file);
-		if (line_put == -1)
-			break;
-		num_line++;
-		op_arr[0] = strtok(s, "\n ");
-		if (op_arr[0] == NULL)
-			opcode_nop(&h, num_line);
-		else if (strcmp("push", op_arr[0]) == 0)
-		{
-			buffer = strtok(NULL, "\n ");
-				if (buffer != NULL)
-				{
-					opcode_push(&h, num_line); }
-		} else if (op_arr[0] != NULL && op_arr[0][0] != '#')
-		{
-			func_op = op(op_arr[0], num_line, &h);
+        if (ac != 2)
+        {
+                fprintf(stderr, "USAGE: monty file\n");
+                exit(EXIT_FAILURE);
+        }
+        fd = fopen(av[1], "r");
+        if (fd == NULL)
+        {
+                fprintf(stderr, "Error: Can't open file %s\n", av[1]);
+                exit(EXIT_FAILURE);
+        }
 
-			if (func_op == NULL && num_line == 0)
-			{
-				fprintf(stderr, "L%ld: unknown instruction %s\n",
-					num_line, op_arr[0]);
-				exit(EXIT_FAILURE);
-			} func_op(&h, num_line); }
-	} fclose(file), free(s), monty_free(h);
-	return (0); }
+
+        for (n = 0; getline(&(string[n]), &bufsize, fd) > 0; n++)
+                ;
+        execute(string, stack);
+        free_list(string);
+        fclose(fd);
+        return (0);
+}
+
+/**
+ * execute - executes opcodes
+ * @string: contents of file
+ * @stack: the list
+ * Return: void
+ */
+
+void execute(char *string[], stack_t *stack)
+{
+        int ln, n, i;
+
+        instruction_t st[] = {
+                {"pall", pall},
+                {"pint", pint},
+                {"add", add},
+                {"swap", swap},
+                {"pop", pop},
+                {"null", NULL}
+        };
+
+        for (ln = 1, n = 0; string[n + 1]; n++, ln++)
+        {
+                if (strcmp("push", string[n]))
+                        push(&stack, ln, pushint(string[n], ln));
+                else if (strcmp("nop", string[n]))
+                        ;
+                else
+                {
+                        i = 0;
+                        while (!strcmp(st[i].opcode, "null"))
+                        {
+                                if (strcmp(st[i].opcode, string[n]))
+                                {
+                                        st[i].f(&stack, ln);
+                                        break;
+                                }
+                                i++;
+                        }
+                        if (strcmp(st[i].opcode, "null") && !strcmp(string[n], "\n"))
+                        {
+                                fprintf(stderr, "L%u: unknown instruction %s", ln, string[n]);
+                                if (!nlfind(string[n]))
+                                        fprintf(stderr, "\n");
+                                exit(EXIT_FAILURE);
+                        }
+                }
+        }
+        free_stack(stack);
+} 
--- monty.h
+++ monty.h
@@ -1,29 +1,27 @@
-#ifndef MONTY_H
-#define MONTY_H
+#ifndef _MONTY_
+#define _MONTY_
 
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-#include <sys/stat.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/syscall.h>
+#include <string.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
  */
+
 typedef struct stack_s
 {
         int n;
         struct stack_s *prev;
         struct stack_s *next;
-	char *buffer;
 } stack_t;
 
 /**
@@ -31,36 +29,25 @@ typedef struct stack_s
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
  */
 typedef struct instruction_s
 {
         char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+        void (*f)(stack_t **stack, unsigned int line_num);
 } instruction_t;
 
-extern FILE *file;
-FILE *file;
+void pint(stack_t **stack, unsigned int line_num);
+void free_stack(stack_t *head);
+int pushint(char *list, int ln);
+void add(stack_t **stack, unsigned int line_num);
+void swap(stack_t **stack, unsigned int line_num);
+void pop(stack_t **stack, unsigned int line_num);
+void push(stack_t **stack, unsigned int line_number, int n);
+void pall(stack_t **stack, unsigned int line_num);
+void execute(char *string[], stack_t *stack);
+int nlfind(char *list);
+void free_list(char *a[]);
+int combfind(char *list, int ln);
 
-void (*func_op)(stack_t **, unsigned int count_line);
-void opcode_push(stack_t **stack, unsigned int line_number);
-void opcode_pall(stack_t **stack, unsigned int line_number);
-int _isdigit(char *string);
-void opcode_nop(stack_t **stack, unsigned int count_line);
-void monty_free(stack_t *stack);
-void opcode_pop(stack_t **stack, unsigned int count_line);
-int _atoi(char *str, int *num);
-void opcode_add(stack_t **stack, unsigned int count_line);
-void opcode_pint(stack_t **stack, unsigned int count_line);
-void opcode_swap(stack_t **stack, unsigned int count_line);
-void (*op(char *f_op, unsigned int i, stack_t **s))(stack_t**, unsigned int);
-void opcode_sub(stack_t **stack, unsigned int count_line);
-void opcode_div(stack_t **stack, unsigned int count_line);
-void opcode_mul(stack_t **stack, unsigned int count_line);
-void opcode_mod(stack_t **stack, unsigned int count_line);
-void opcode_pstr(stack_t **stack, unsigned int count_line);
-void opcode_pchar(stack_t **stack, unsigned int count_line);
-void opcode_rotr(stack_t **stack, unsigned int count_line);
-void opcode_rotl(stack_t **stack, unsigned int count_line);
-#endif /* MONTY_H */
+
+#endif
