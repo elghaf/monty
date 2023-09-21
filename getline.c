d7d5a51f30a4c0cd1983d22aa434444d49320945
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-09-22 00:51:15 +0100

    add

--- getline.c
+++ getline.c
@@ -1,49 +1,59 @@
 #include "monty.h"
 
 /**
- * line_tok - Gets the line from the monty file and tokenizes it.
- * @head: The head of the linked list
- * @fp: File pointer
- * @line: Line from the monty file to tokenize
- * @line_number: The current working line number of the Monty bytecodes file
- * @mode: 0 if stack, 1 if queue
- * Return: void
+ * tokenize_and_process_line - Tokenizes and processes a line from the Monty file.
+ * @stack_head: The head of the linked list (stack).
+ * @file_ptr: File pointer to the Monty bytecode file.
+ * @line_buffer: Line from the Monty file to tokenize.
+ * @line_number: The current line number of the Monty bytecodes file.
+ * @execution_mode: 0 if stack mode, 1 if queue mode.
+ *
+ * Description:
+ * This function tokenizes a line from the Monty file and processes it based on
+ * the execution mode (stack or queue). It also handles mode-changing commands.
  */
-void line_tok(stack_t **head, FILE *fp, char *line,
-unsigned int line_number, int *mode)
+void tokenize_and_process_line(stack_t **stack_head, FILE *file_ptr, char *line_buffer,
+                               unsigned int line_number, int *execution_mode)
 {
 	char *token = NULL;
 	int mode_changed = 0;
 
-	token = strtok(line, " \n");
+	token = strtok(line_buffer, " \n");
 	while (token && token[0] == ' ')
 		token = strtok(NULL, " \n");
+
 	if (!token)
 		return;
-	mode_changed = check_mode(token, mode);
+
+	mode_changed = check_mode(token, execution_mode);
+
 	if (mode_changed)
 		return;
-	check_token(head, fp, line, token, line_number, mode);
+
+	process_token(stack_head, file_ptr, line_buffer, token, line_number, execution_mode);
 }
+
 /**
- * check_mode - Checks if the mode has been changed to stack or queue
- * @token: The word to check
- * @mode: 0 if stack, 1 if queue
- * Return: 1 if changed, 0 if not changed
+ * check_mode - Checks if the mode has been changed to stack or queue.
+ * @token: The word to check for mode change.
+ * @execution_mode: Pointer to the execution mode (0 if stack, 1 if queue).
+ * Return: 1 if mode changed, 0 if not changed.
  */
-int check_mode(char *token, int *mode)
+int check_mode(char *token, int *execution_mode)
 {
 	if (!strncmp(token, "stack", 5) &&
 			(token[5] == '\0' || token[5] == ' ' || token[5] == '\n'))
 	{
-		*mode = 0;
+		*execution_mode = 0;
 		return (1);
 	}
+
 	if (!strncmp(token, "queue", 5) &&
 			(token[5] == '\0' || token[5] == ' ' || token[5] == '\n'))
 	{
-		*mode = 1;
+		*execution_mode = 1;
 		return (1);
 	}
+
 	return (0);
 }
--- mod.c
+++ mod.c
@@ -1,29 +1,37 @@
 #include "monty.h"
 
 /**
- * _mod - Computes the modulus division of the second top element
- *	  of the stack by the top element of the stack
- * @head: A pointer to the top of the stack
- * @line_number: The current line number of the bytecode file
- * Return: void
+ * compute_modulus - Computes the modulus division of the second top element
+ *                  of the stack by the top element of the stack.
+ * @stack_head: A pointer to the top of the stack.
+ * @line_number: The current line number of the bytecode file.
+ *
+ * Description:
+ * This function computes the modulus division of the second top element of the
+ * stack by the top element and replaces the second top element with the result.
+ * If the stack is too short or if division by zero is attempted, it prints
+ * corresponding error messages.
  */
-void _mod(stack_t **head, unsigned int line_number)
+void compute_modulus(stack_t **stack_head, unsigned int line_number)
 {
-	int mod = 0;
+	int modulus = 0;
 
-	if (*head == 0 || (*head)->next == 0)
+	if (!stack_head || !*stack_head || !(*stack_head)->next)
 	{
 		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
 		error = 1;
 		return;
 	}
-	if ((*head)->n == 0)
+
+	if ((*stack_head)->n == 0)
 	{
 		fprintf(stderr, "L%d: division by zero\n", line_number);
 		error = 1;
 		return;
 	}
-	mod = (((*head)->next)->n) % ((*head)->n);
-	_pop(head, line_number);
-	(*head)->n = mod;
+
+	modulus = ((*stack_head)->next->n) % ((*stack_head)->n);
+	pop_top_element(stack_head, line_number);
+	(*stack_head)->n = modulus;
 }
+
