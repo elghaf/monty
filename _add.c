57fe0c00b40fad7c42c36c6cbf55f9195d6aad39
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-09-22 02:02:28 +0100

    Update _add.c

--- _add.c
+++ _add.c
@@ -21,9 +21,9 @@ void add_elements_to_stack(stack_t **stack_head, unsigned int line_number)
     if (stack_length < 2)
     {
         fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
-        fclose(bus.file); // Assuming bus.file is a global variable
-        free(bus.content); // Assuming bus.content is a global variable
-        free_stack(*stack_head); // Assuming free_stack is a function to free the stack
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*stack_head);
         exit(EXIT_FAILURE);
     }
 
