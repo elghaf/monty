f98c44df74487c605dcec1084cce73fee93988be
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-09-24 22:20:11 +0100

    add

--- nopp.c
+++ nopp.c
@@ -1,8 +1,8 @@
 #include "monty.h"
 /**
- * nop - Do none.
- * @stack: Double pointer of the stacks.
- * @l_num: The line number of files.
+ * nop - Does nothing.
+ * @stack: Double pointer to the beginning of the stack.
+ * @l_num: The line number in the Monty bytecode file.
  */
 
 void nop(stack_t **stack, unsigned int l_num)
