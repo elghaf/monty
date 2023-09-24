e59a21bc80ac5fdb966d33b5cabf2cecba1f5025
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-09-24 23:03:41 +0100

    Update pint.c

--- pint.c
+++ pint.c
@@ -8,7 +8,7 @@
  */
 void pint(stack_t **stack_beg, unsigned int l_num)
 {
-	if (stack_beg == NULL || *stack_beg == NULL)
+	if (*stack_beg == NULL || stack_beg == NULL)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", l_num);
 		free(data);
