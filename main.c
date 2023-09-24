65676dde69f5845b2aee3892298fcd6c8dcdcdb8
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-09-25 00:49:42 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,49 +1,49 @@
 #include "monty.h"
-bus_t bus = {NULL, NULL, NULL, 0};
+bus_t monty_bus = {NULL, NULL, NULL, 0};
+
 /**
- * main - monty code interpreter
+ * main - Monty code interpreter
  * @argc: number of arguments
- * @argv: monty file location
+ * @argv: Monty file location
  * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-	char *content;
-	FILE *file;
-	size_t size = 0;
-	ssize_t read_line = 1;
-	stack_t *stack = NULL;
-	UINT counter = 0;
+    char *line_content;
+    FILE *monty_file;
+    size_t line_size = 0;
+    ssize_t read_line = 1;
+    stack_t *stack = NULL;
+    unsigned int line_counter = 0;
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-	file = fopen(argv[1], "r");
-	bus.file = file;
-	if (!file)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	while (read_line > 0)
-	{
-		content = NULL;
-		read_line = getline(&content, &size, file);
-		bus.content = content;
-		counter++;
-		if (read_line > 0)
-		{
-			execute(content, &stack, counter, file);
-		}
-		free(content);
-	}
-	free_stack(stack);
-	fclose(file);
-	return (0);
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+    monty_file = fopen(argv[1], "r");
+    monty_bus.file = monty_file;
+    if (!monty_file)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+    while (read_line > 0)
+    {
+        line_content = NULL;
+        read_line = getline(&line_content, &line_size, monty_file);
+        monty_bus.content = line_content;
+        line_counter++;
+        if (read_line > 0)
+        {
+            execute(line_content, &stack, line_counter, monty_file);
+        }
+        free(line_content);
+    }
+    free_stack(stack);
+    fclose(monty_file);
+    return (0);
 }
-
 
 #include "monty.h"
 /**
@@ -52,13 +52,13 @@ int main(int argc, char *argv[])
  */
 void free_stack(stack_t *head)
 {
-	stack_t *aux;
+    stack_t *aux;
 
-	aux = head;
-	while (head)
-	{
-		aux = head->next;
-		free(head);
-		head = aux;
-	}
+    aux = head;
+    while (head)
+    {
+        aux = head->next;
+        free(head);
+        head = aux;
+    }
 }
