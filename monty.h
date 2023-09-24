59c4fb78a04a33a4e074b4dec8266b306a16541e
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-09-24 20:38:35 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -54,15 +54,16 @@ void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
-void add(stack_t **stack, unsigned int line_number);
-void sub(stack_t **stack, unsigned int line_number);
-void divide(stack_t **stack, unsigned int line_number);
-void mul(stack_t **stack, unsigned int line_number);
-void mod(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void pchar(stack_t **stack, unsigned int line_number);
 void pstr(stack_t **stack, unsigned int line_number);
 void rotl(stack_t **stack, unsigned int line_number);
 void rotr(stack_t **stack, unsigned int line_number);
+
+void add_stack(stack_t **stk, unsigned int line_num);
+void sub_stack(stack_t **stk, unsigned int line_num);
+void divide_stack(stack_t **stk, unsigned int line_num);
+void mul_stack(stack_t **stk, unsigned int line_num);
+void mod_stack(stack_t **stk, unsigned int line_num);
 
 #endif
