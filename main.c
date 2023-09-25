c7db2d22020d7e13ce52c9be540dae924df3fb7a
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-09-25 03:11:36 +0100

    add

--- main.c
+++ main.c
@@ -1,108 +1,83 @@
 #include "monty.h"
-
-handle_t handle = {NULL, NULL, NULL};
+stack_t *head = NULL;
 
 /**
- * function_main - function that chooses the opcode
- *
- * @line: string containing the line
- * @head: head of the stack
- * @line_num: line number
- * Return: 0 or 1
+ * main - entry point
+ * @argc: arguments count
+ * @argv: list of arguments
+ * Return: always 0
  */
-int function_main(char *line, stack_t **head, unsigned int line_num)
+
+int main(int argc, char *argv[])
 {
-    unsigned int i = 0;
-    char *delimiter = " \t\n\r", *str_tok;
-    instruction_t arr[] = {
-        {"push", push}, {"pint", pint},
-        {"pall", pall}, {"pop", pop},
-        {"swap", swap}, {"add", add}, {"nop", nop},
-        {"sub", sub},{NULL, NULL}
-    };
-    str_tok = strtok(line, delimiter);
-    if (str_tok && str_tok[0] == '#')
-        return (0);
-    handle.num = strtok(NULL, delimiter);
-    while (arr[i].opcode && str_tok)
-    {
-        if (strcmp(arr[i].opcode, str_tok) == 0)
-        {
-            arr[i].f(head, line_num);
-            return (0);
-        }
-        i++;
-    }
-    if (str_tok && arr[i].opcode == NULL)
-    {
-        fprintf(stderr, "L%d: unknown instruction %s\n", line_num, str_tok);
-        fclose(handle.textfile);
-        free(line);
-        frees_the_stack(*head);
-        exit(EXIT_FAILURE);
-    }
-    return (1);
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	open_file(argv[1]);
+	free_nodes();
+	return (0);
 }
 
 /**
- * frees_the_stack - function that frees our stack
- * @head: head of our stack
+ * create_node - Creates a node.
+ * @n: Number to go inside the node.
+ * Return: Upon sucess a pointer to the node. Otherwise NULL.
  */
-void frees_the_stack(stack_t *head)
+stack_t *create_node(int n)
 {
-    stack_t *ptr;
+	stack_t *node;
 
-    ptr = head;
-    while (head)
-    {
-        ptr = head->next;
-        free(head);
-        head = ptr;
-    }
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+		err(4);
+	node->next = NULL;
+	node->prev = NULL;
+	node->n = n;
+	return (node);
 }
 
 /**
- * main - main function
- *
- * @argc: number of arguments to function main
- * @argv: array of pointers to char
- * Return: int
+ * free_nodes - Frees nodes in the stack.
  */
-int main(int argc, char **argv)
+void free_nodes(void)
 {
-    char *line;
-    stack_t *head = NULL;
-    size_t n = 0;
-    int i = 1;
-    unsigned int line_num = 0;
-    FILE *textfile;
+	stack_t *tmp;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
-    textfile = fopen(argv[1], "r");
-    if (textfile == NULL)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
-    while (i > 0)
-    {
-        line = NULL;
-        line_num++;
-        handle.textfile = textfile;
-        i = getline(&line, &n, textfile);
-        handle.str = line;
-        if (i > 0)
-        {
-            function_main(line, &head, line_num);
-        }
-        i++;
-        free(line);
-    }
-    fclose(textfile);
-    frees_the_stack(head);
-    return (0);
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
+
+
+/**
+ * add_to_queue - Adds a node to the queue.
+ * @new_node: Pointer to the new node.
+ * @ln: line number of the opcode.
+ */
+void add_to_queue(stack_t **new_node, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
+
+	if (new_node == NULL || *new_node == NULL)
+		exit(EXIT_FAILURE);
+	if (head == NULL)
+	{
+		head = *new_node;
+		return;
+	}
+	tmp = head;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = *new_node;
+	(*new_node)->prev = tmp;
+
 }
