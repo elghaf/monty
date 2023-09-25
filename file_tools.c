2cfc362ed224ff839e0baa848bfcfaa7275242a4
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-09-25 03:16:07 +0100

    Update file_tools.c

--- file_tools.c
+++ file_tools.c
@@ -90,12 +90,6 @@ void find_func(char *opcode, char *value, int ln, int format)
 		{"pint", print_top},
 		{"pop", pop_top},
 		{"nop", nop},
-		{"swap", swap_nodes},
-		{"add", add_nodes},
-		{"sub", sub_nodes},
-		{"div", div_nodes},
-		{"mul", mul_nodes},
-		{"mod", mod_nodes},
 		{NULL, NULL}
 	};
 
