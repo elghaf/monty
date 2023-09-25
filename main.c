50c0e1c989b070b0e11f1dadb04a8a6e59089160
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-09-25 01:34:30 +0100

    add

--- main.c
+++ main.c
@@ -1,82 +1,45 @@
 #include "monty.h"
-stack_t *stack = NULL;
-
+bus_t bus = {NULL, NULL, NULL, 0};
 /**
- * main - Entry point of the program
- * @argc: Number of command line arguments
- * @argv: Array of command line argument strings
- * Return: Always returns 0
- */
-
+* main - monty code interpreter
+* @argc: number of arguments
+* @argv: monty file location
+* Return: 0 on success
+*/
 int main(int argc, char *argv[])
 {
+	char *content;
+	FILE *file;
+	size_t size = 0;
+	ssize_t read_line = 1;
+	stack_t *stack = NULL;
+	unsigned int counter = 0;
+
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	open_and_parse_file(argv[1]);
-	free_stack();
-	return (0);
-}
-
-/**
- * create_node - Creates a new stack node
- * @value: Value to be stored in the new node
- * Return: A pointer to the newly created node, or NULL on failure
- */
-stack_t *create_node(int value)
-{
-	stack_t *new_node;
-
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
-		exit(EXIT_FAILURE);
-
-	new_node->next = NULL;
-	new_node->prev = NULL;
-	new_node->n = value;
-	return (new_node);
-}
-
-/**
- * free_stack - Frees all nodes in the stack
- */
-void free_stack(void)
-{
-	stack_t *current_node;
-
-	if (stack == NULL)
-		return;
-
-	while (stack != NULL)
+	file = fopen(argv[1], "r");
+	bus.file = file;
+	if (!file)
 	{
-		current_node = stack;
-		stack = stack->next;
-		free(current_node);
-	}
-}
-
-/**
- * add_to_stack - Adds a new node to the top of the stack
- * @new_node: Pointer to the new node
- * @line_number: Line number where the opcode appears
- */
-void add_to_stack(stack_t **new_node, __attribute__((unused)) unsigned int line_number)
-{
-	stack_t *temp;
-
-	if (new_node == NULL || *new_node == NULL)
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
-
-	if (stack == NULL)
-	{
-		stack = *new_node;
-		return;
 	}
-
-	temp = stack;
-	stack = *new_node;
-	(*new_node)->next = temp;
-	temp->prev = *new_node;
+	while (read_line > 0)
+	{
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		bus.content = content;
+		counter++;
+		if (read_line > 0)
+		{
+			execute(content, &stack, counter, file);
+		}
+		free(content);
+	}
+	free_stack(stack);
+	fclose(file);
+return (0);
 }
